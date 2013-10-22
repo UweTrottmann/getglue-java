2d86a8f92f37dd7359f92b26d27a6d78f4b878c7
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-10-22 17:08:01 +0200

    Fix auth grant request.
    - Add info in query instead of to the body, duh.

--- src/main/java/com/uwetrottmann/getglue/ServiceManager.java
+++ src/main/java/com/uwetrottmann/getglue/ServiceManager.java
@@ -60,7 +60,7 @@ public class ServiceManager {
                 .setClientSecret(clientSecret)
                 .setRedirectURI(redirectUri)
                 .setCode(authCode)
-                .buildBodyMessage();
+                .buildQueryMessage();
         return request;
     }
 
