6e0364497ae06aa910fb48eeb3ea617367b4e2cf
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-10-25 10:57:18 +0200

    Add description to GetGlueCheckIn id field.

--- src/main/java/com/uwetrottmann/getglue/entities/GetGlueCheckIn.java
+++ src/main/java/com/uwetrottmann/getglue/entities/GetGlueCheckIn.java
@@ -2,6 +2,9 @@ package com.uwetrottmann.getglue.entities;
 
 public class GetGlueCheckIn {
 
+    /**
+     * The object id of the interaction related to the check-in.
+     */
     public String id;
 
 }
