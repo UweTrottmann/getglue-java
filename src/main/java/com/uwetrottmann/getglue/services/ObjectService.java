74c5d87f9616f2443a507d966dd25974d64370f7
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-10-25 11:27:35 +0200

    Add object feed and like endpoints, simpler name for others.

--- src/main/java/com/uwetrottmann/getglue/services/ObjectService.java
+++ src/main/java/com/uwetrottmann/getglue/services/ObjectService.java
@@ -15,30 +15,67 @@
  */
 package com.uwetrottmann.getglue.services;
 
-import com.uwetrottmann.getglue.entities.GetGlueCheckIn;
+import com.uwetrottmann.getglue.entities.GetGlueInteraction;
 import com.uwetrottmann.getglue.entities.GetGlueObjectResource;
-import retrofit.http.*;
+
+import java.util.List;
+
+import retrofit.http.EncodedPath;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.GET;
+import retrofit.http.POST;
 
 /**
  * Endpoints for <a href="http://developer.getglue.com/#object-resources">Object Resources</a>.
  */
 public interface ObjectService {
 
+    /**
+     * Retrieve a GetGlue object. Can be any TV show, movie, sports team, game, or episode.
+     */
     @GET("/{object-id}")
-    GetGlueObjectResource getObject(
+    GetGlueObjectResource get(
             @EncodedPath("object-id") String objectId
     );
 
+    /**
+     * Creates a new check-in to the object with the authenticated user. Returns an Interaction
+     * resource containing at least an id.
+     */
     @POST("/{object-id}/checkins")
-    GetGlueCheckIn checkinObject(
+    GetGlueInteraction checkin(
             @EncodedPath("object-id") String objectId
     );
 
+    /**
+     * Creates a new check-in to the object with the authenticated user. Returns an Interaction
+     * resource containing at least an id.
+     */
     @FormUrlEncoded
     @POST("/{object-id}/checkins")
-    GetGlueCheckIn checkinObject(
+    GetGlueInteraction checkin(
             @EncodedPath("object-id") String objectId,
             @Field("comment") String comment
+    );
+
+    /**
+     * Retrieve a pageable collection of all feed interactions around the specified object. A feed
+     * is a collection of curated interactions selected from the set of all interactions. Returns an
+     * array of Interaction resources.
+     */
+    @GET("/{object-id}/feed")
+    List<GetGlueInteraction> feed(
+            @EncodedPath("object-id") String objectId
+    );
+
+    /**
+     * Creates a new like for the object with the authenticated user. Returns an Interaction object
+     * containing at least an id.
+     */
+    @POST("/{object-id}/likes")
+    GetGlueInteraction like(
+            @EncodedPath("object-id") String objectId
     );
 
 }
--- src/test/java/com/uwetrottmann/getglue/services/ObjectServiceTest.java
+++ src/test/java/com/uwetrottmann/getglue/services/ObjectServiceTest.java
@@ -1,16 +1,18 @@
 package com.uwetrottmann.getglue.services;
 
 import com.uwetrottmann.getglue.BaseTestCase;
-import com.uwetrottmann.getglue.entities.GetGlueCheckIn;
+import com.uwetrottmann.getglue.entities.GetGlueInteraction;
 import com.uwetrottmann.getglue.entities.GetGlueObjectResource;
+
+import java.util.List;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class ObjectServiceTest extends BaseTestCase {
 
-    public void test_getObject() {
+    public void test_get() {
         ObjectService service = getManager().objectService();
-        GetGlueObjectResource resource = service.getObject("tv_shows/glee");
+        GetGlueObjectResource resource = service.get("tv_shows/glee");
         assertThat(resource).isNotNull();
         assertThat(resource.object).isNotNull();
         assertThat(resource.object).isNotNull();
@@ -18,16 +20,32 @@ public class ObjectServiceTest extends BaseTestCase {
         assertThat(resource.object.title).isEqualTo("Glee");
     }
 
-    public void test_checkinObject() {
+    public void test_checkin() {
         ObjectService service = getManager().objectService();
-        GetGlueCheckIn resource = service.checkinObject("tv_shows/glee");
+        GetGlueInteraction resource = service.checkin("tv_shows/glee");
         assertThat(resource).isNotNull();
         assertThat(resource.id).isNotEmpty();
     }
 
-    public void test_checkinObjectComment() {
+    public void test_checkinWithComment() {
         ObjectService service = getManager().objectService();
-        GetGlueCheckIn resource = service.checkinObject("tv_shows/how_i_met_your_mother", "Testing getglue-java.");
+        GetGlueInteraction resource = service.checkin("tv_shows/how_i_met_your_mother",
+                "Testing getglue-java.");
+        assertThat(resource).isNotNull();
+        assertThat(resource.id).isNotEmpty();
+    }
+
+    public void test_feed() {
+        ObjectService service = getManager().objectService();
+        List<GetGlueInteraction> interactions = service.feed("tv_shows/glee");
+        assertThat(interactions).isNotEmpty();
+        assertThat(interactions.get(0)).isNotNull();
+        assertThat(interactions.get(0).id).isNotEmpty();
+    }
+
+    public void test_like() {
+        ObjectService service = getManager().objectService();
+        GetGlueInteraction resource = service.like("tv_shows/glee");
         assertThat(resource).isNotNull();
         assertThat(resource.id).isNotEmpty();
     }
